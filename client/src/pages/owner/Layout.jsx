37c6885156f7a33f8a3d866ff6756f31ead2606d
Satwik <shikharyadav280203@gmail.com>
2025-08-09 03:51:38 +0530

    Fix case-sensitive import for NavbarOwner to support Vercel build

--- client/src/pages/owner/Layout.jsx
+++ client/src/pages/owner/Layout.jsx
@@ -1,5 +1,5 @@
 import React,{useEffect} from 'react'
-import NavbarOwner from '../../components/owner/NavBarOwner'
+import NavbarOwner from '../../components/owner/NavbarOwner'
 import Sidebar from '../../components/owner/Sidebar'
 import { Outlet } from 'react-router-dom'
 import {useAppContext} from '../../context/AppContext'
